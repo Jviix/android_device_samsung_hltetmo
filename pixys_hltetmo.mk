8710283bb6dbf2dafe2e162e185bd7b73318c6c9
Jviix <33033405+Jviix@users.noreply.github.com>
2018-09-18 18:04:39 +0200

    Update pixys_hltetmo.mk

--- pixys_hltetmo.mk
+++ pixys_hltetmo.mk
@@ -14,12 +14,12 @@
 # limitations under the License.
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/hltetmo/full_hltetmo.mk)
 
 PRODUCT_DEVICE := hltetmo
-PRODUCT_NAME := pixys_hltetmo
+PRODUCT_NAME := liquid_hltetmo
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_ARCH := arm
